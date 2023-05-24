aa777f4cf6574396aa013024cb524acc1e5dcd00
Petr Kim <ppeximo@gmail.com>
2023-05-24 13:20:05 +0300

    Server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,29 @@
-import java.io.File;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
+
+            try (ServerSocket serverSocket = new ServerSocket(8989)) {
+                BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
+                while (true) {
+                    try (Socket clientSocket = serverSocket.accept();
+                         BufferedReader reader = new BufferedReader
+                                 (new InputStreamReader(clientSocket.getInputStream()))) {
+                        PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+                        List<PageEntry> searchingResult = engine.search(reader.readLine());
+                        writer.println(new Gson().toJson(searchingResult));
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Не могу стартовать сервер");
+                e.printStackTrace();
+            }
+        }
 
         // здесь создайте сервер, который отвечал бы на нужные запросы
         // слушать он должен порт 8989
